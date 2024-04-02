ed1e6662e0ce38f0dd54097482654bf78c0c198c
Thiago Costa <thiagorpc@hotmail.com>
2024-04-01 21:57:18 -0300

    Criação do repositório no Git

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align="center">PUCRS | Fundamentos de Sistemas Web</h1>
+
+<p align="center">
+MeuPet MeuDoce - Plataforma online para PetShop. 
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-execução">Execução</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="PUCRS | Fundamentos de Sistemas Web" src=".github/preview.jpeg" width="90%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- HTML
+- JavaScript
+- CSS
+- Git e Github
+
+## 💻 Escopo do Projeto
+
+O Projeto consiste na criação de uma plataforma online para um PetShop, abrangendo todas as fases de planejamento e desenvolvimento, conforme abordado durante as aulas.
+
+A plataforma online é um sistema web de comércio de produtos e serviços para PetShop. Os produtos disponíveis para venda são ração seca e ração úmida, e o aluno tem a liberdade de escolher as opções de apresentação, desde que haja pelo menos duas opções de cada categoria. Quanto aos serviços, eles são divididos em banho e tosa, com categorias para animais de pequeno porte e de grande porte.
+
+## 🔖 Execução
+
+Para visualizar o projeto basta acessar:
+https://thiagorpc.github.com/pucrs-fundamentos-web-projeto-parte02/
+
+Para baixar os arquivos localmente:
+
+```
+git clone https://github.com/thiagorpc/pucrs-fundamentos-web-projeto-parte02.git
+```
+
+## 📝 Licença
+
+Esse projeto está sob a licença MIT.
---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+// Controle do Menu
+function oculatarMenu() {
+  document.getElementById("subMenu").classList.toggle("subMenuOcultar");
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const el = document.getElementById("responsive-menu");
+  if (el) {
+    el.addEventListener("click", oculatarMenu, false);
+  }
+});
+
+//##################################################################
+// Leaf Map
+// Where you want to render the map.
+var element = document.getElementById("osm-map");
+
+// Height has to be set. You can do this in CSS too.
+element.style = "height:500px;";
+
+// Create Leaflet map on map element.
+var map = L.map(element);
+
+// Add OSM tile layer to the Leaflet map.
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// Target's GPS coordinates.
+var target = L.latLng("-30.059297", "-51.172996");
+
+// Set map's center to target with zoom 14.
+map.setView(target, 17);
+
+// Place a marker on the same location.
+L.marker(target).addTo(map);
+
+//##################################################################
+// Faligação de Formulário
+
+const fields = document.querySelectorAll("[required]");
+//console.log(fields)
+
+function customValidation(event) {
+  const field = event.target;
+
+  //logica para verificar se existem erros
+  function verifyErrors() {
+    let foundError = false;
+
+    for (let error in field.validity) {
+      //se nao for customError
+      // entao verifica se tem erro
+      if (error != "customError" && field.validity[error]) {
+        foundError = error;
+      }
+    }
+    return foundError;
+  }
+
+  const error = verifyErrors();
+
+  if (error) {
+    //trocar mensagem padrao
+    field.setCustomValidity("Campo é obrigatório");
+  } else {
+    field.setCustomValidity("");
+  }
+}
+
+for (field of fields) {
+  field.addEventListener("invalid", customValidation);
+}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  console.log("Enviar o formulario");
+  //Proteção para não enviar o formulário se estiver com dados padrão
+  event.preventDefault();
+});
